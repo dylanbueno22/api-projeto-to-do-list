ae8e2d939f05afb92cecd7d3ff5173ab7f72b031
dylanbueno22 <dylanbueno22@gmail.com>
2025-09-01 15:50:47 -0300

    test: temporarily disable database to test API on Vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { GroupsModule } from './groups/groups.module';
-import { databaseConfig } from './config/database.config';
-import { vercelConfig } from './config/vercel.config';
+// import { databaseConfig } from './config/database.config';
+// import { vercelConfig } from './config/vercel.config';
 import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(process.env.VERCEL ? vercelConfig : databaseConfig),
+    // TypeOrmModule.forRoot(process.env.VERCEL ? vercelConfig : databaseConfig), // Temporariamente desabilitado
     TasksModule,
     GroupsModule,
   ],
